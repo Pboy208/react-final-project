d81e07cc2feb3c6bea467e40f9cb2d5b5387882b
pboy208 <phuonghoanghaha@gmail.com>
2022-03-22 09:27:00 +0700

    feat: add Global style for styled-component

---
+++ src/components/common/GlobalStyle.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+const themes = {
+    light: {
+        body: "#FFF",
+        background: "white",
+        text: "#363537",
+        borderColor: "#363537",
+    },
+    dark: {
+        body: "#5C3D2E",
+        background: "rgb(0, 30, 60)",
+        text: "#FAFAFA",
+        borderColor: "#FAFAFA",
+    },
+};
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background: ${({ theme }) => themes[theme].body};
+    color: ${({ theme }) => themes[theme].text};
+    border-color: ${({ theme }) => themes[theme].borderColor} !important;
+  }
+`;
+
+export default GlobalStyle;
