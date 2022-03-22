afabd520d30ab0a0c53fc172eb380ae8903aae9a
pboy208 <phuonghoanghaha@gmail.com>
2022-03-22 20:01:52 +0700

    feat: add token detection when initiating app

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,11 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import decode from "jwt-decode";
+
+const initialState = {
+    isLoggedIn: false,
+    userName: "",
+};
+
+const isTokenValid = (token) => {
+    if (!token) return false;
+    const decodedToken = decode(token);
+    const remainTimeInSeconds = decodedToken.exp - Math.floor(Date.now() / 1000);
+    return remainTimeInSeconds > 0 ? true : false;
+};
+
+const token = localStorage.getItem("token");
+
+if (isTokenValid(token)) {
+    initialState.isLoggedIn = true;
+    initialState.userName = decode(token).userName;
+    console.log("set user", initialState.userName);
+}
+
 const authSlice = createSlice({
     name: "auth",
-    initialState: {
-        isLoggedIn: false,
-        userName: "",
-    },
+    initialState,
     reducers: {
         logIn(state, action) {
             state.isLoggedIn = true;
