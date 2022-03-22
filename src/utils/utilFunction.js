1c0726b35a01bc0e5caacee66cb4e00232f3ee26
pboy208 <phuonghoanghaha@gmail.com>
2022-03-22 23:09:15 +0700

    feat: add create request helper func + add authentication api

--- src/utils/utilFunction.js
+++ src/utils/utilFunction.js
@@ -1,1 +1,34 @@
 export const formatVnd = (n) => n.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ".") + " vnd";
+
+const baseUrl = "http://localhost:3000";
+
+const generateUrlWithParams = (givenUrl, params) => {
+    const url = new URL(givenUrl);
+    const urlSearchParams = new URLSearchParams();
+    for (let key in params) {
+        urlSearchParams.set(key, params[key]);
+    }
+    url.search = urlSearchParams;
+    return url;
+};
+
+export const createRequest = ({
+    endpoint,
+    method = "GET",
+    body = null,
+    token = null,
+    params = null,
+}) => {
+    const url = baseUrl + endpoint;
+    const requestUrl = params ? generateUrlWithParams(url, params) : url;
+    const requestConfig = {
+        method,
+        headers: {
+            authorization: token ? `Bearer ${token}` : null,
+            accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: body ? JSON.stringify(body) : null,
+    };
+    return [requestUrl, requestConfig];
+};
