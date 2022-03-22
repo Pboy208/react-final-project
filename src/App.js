430e34c0dd2c2cb58b984ef32aab98646b0f165d
pboy208 <phuonghoanghaha@gmail.com>
2022-03-22 09:43:35 +0700

    feat: add routing to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Layout from "./components/common/Layout";
 import GlobalStyle from "./components/common/GlobalStyle";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
     return (
         <>
             <GlobalStyle theme="light" />
-            <Layout>Testing</Layout>
+            <Layout>
+                <Routes>
+                    <Route path="/" exact element={<Navigate to="/home" />} />
+                    <Route path="/login" exact element={<div>login</div>} />
+                    <Route path="/register" exact element={<div>register</div>} />
+                    <Route path="/home" exact element={<div>home</div>} />
+                    <Route path="/product/create" exact element={<div>create</div>} />
+                    <Route path="/product/:productId" exact element={<div>view</div>} />
+                    <Route path="/*" element={<div>not found</div>} />
+                </Routes>
+            </Layout>
         </>
     );
 };
