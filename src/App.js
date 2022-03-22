8e1f5095d9514299c1d80ea58d9f29ff7c1ff33d
pboy208 <phuonghoanghaha@gmail.com>
2022-03-22 16:03:14 +0700

    feat: add PageNotFound for '/*' route

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import ProductForm from "./components/common/ProductForm";
 import AddProduct from "./components/AddProduct";
 import UpdateProduct from "./components/UpdateProduct";
+import PageNotFound from "./components/PageNotFound";
 const App = () => {
     return (
         <>
@@ -19,7 +19,7 @@ const App = () => {
                     <Route path="/home" exact element={<Home />} />
                     <Route path="/product/create" exact element={<AddProduct />} />
                     <Route path="/product/:productId" exact element={<UpdateProduct />} />
-                    <Route path="/*" element={<div>not found</div>} />
+                    <Route path="/*" element={<PageNotFound />} />
                 </Routes>
             </Layout>
         </>
