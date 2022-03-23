1d546a4da24b04cbc4b581e5046d23f971be9584
pboy208 <phuonghoanghaha@gmail.com>
2022-03-23 10:22:00 +0700

    feat: add getProductListThunk

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,4 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { normalize } from "normalizr";
+import * as normalizerSchema from "../utils/schemas/normalizr-schema";
+import { thunkWrapper } from "../utils/utilFunction";
+import * as productApi from "../api/productApi";
 
 const productSlice = createSlice({
     name: "product",
@@ -30,6 +34,21 @@ const productSlice = createSlice({
             delete state.byIds[action.payload.id];
         },
     },
+});
+
+export const getProductListThunk = thunkWrapper((filter, search) => async (dispatch) => {
+    const response = await fetch(...productApi.getProductList());
+    if (!response.ok) throw response.status;
+
+    const data = (await response.json()).data;
+    const normalizedData = normalize(data, normalizerSchema.arrayOfProduct);
+
+    const normalizedProductList = {
+        byIds: normalizedData.entities.product,
+        ids: normalizedData.result,
+    };
+
+    dispatch(productSlice.actions.replaceProductList(normalizedProductList));
 });
 
 export default productSlice.reducer;
