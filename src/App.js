30547bdbd055e9c1410c4b6fe8ab5f2ca0aabd92
Pboy208 <phuonghoanghaha@gmail.com>
2022-03-25 09:40:31 +0700

    feat: add suspense and UnprotectedRoute for routing when user has logged in

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Login from "./components/Login";
 import PageNotFound from "./components/PageNotFound";
 import { useTheme } from "./context/ThemeContext";
 import ProtectedRoute from "./components/common/ProtectedRoute";
+import UnprotectedRoute from "./components/common/UnprotectedRoute";
 import { ToastContainer } from "@ahaui/react";
+import LoadingSpinner from "./components/common/LoadingSpinner";
+
 const Home = React.lazy(() => import("./components/Home"));
 const Register = React.lazy(() => import("./components/Register"));
 const AddProduct = React.lazy(() => import("./components/AddProduct"));
@@ -14,29 +17,39 @@ const UpdateProduct = React.lazy(() => import("./components/UpdateProduct"));
 
 const App = () => {
     const { theme } = useTheme();
-    console.log("render App");
     return (
         <>
             <GlobalStyle theme={theme} />
             <ToastContainer />
             <Layout>
-                <Routes>
-                    <Route path="/" exact element={<Navigate to="/home" />} />
-                    <Route path="/login" exact element={<Login />} />
-                    <Route path="/register" exact element={<Register />} />
-                    <Route path="/home" exact element={<ProtectedRoute component={Home} />} />
-                    <Route
-                        path="/product/create"
-                        exact
-                        element={<ProtectedRoute component={AddProduct} />}
-                    />
-                    <Route
-                        path="/product/:productId"
-                        exact
-                        element={<ProtectedRoute component={UpdateProduct} />}
-                    />
-                    <Route path="/*" element={<PageNotFound />} />
-                </Routes>
+                <React.Suspense fallback={<LoadingSpinner isLoading={true} />}>
+                    <Routes>
+                        <Route path="/" exact element={<Navigate to="/home" />} />
+                        <Route
+                            path="/login"
+                            exact
+                            element={<UnprotectedRoute component={Login} />}
+                        />
+                        <Route
+                            path="/register"
+                            exact
+                            element={<UnprotectedRoute component={Register} />}
+                        />
+
+                        <Route path="/home" exact element={<ProtectedRoute component={Home} />} />
+                        <Route
+                            path="/product/create"
+                            exact
+                            element={<ProtectedRoute component={AddProduct} />}
+                        />
+                        <Route
+                            path="/product/:productId"
+                            exact
+                            element={<ProtectedRoute component={UpdateProduct} />}
+                        />
+                        <Route path="/*" element={<PageNotFound />} />
+                    </Routes>
+                </React.Suspense>
             </Layout>
         </>
     );
---
+++ src/components/common/UnprotectedRoute.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const UnprotectedRoute = ({ component: Component }) => {
+    const { isLoggedIn } = useSelector((state) => state.auth);
+    return isLoggedIn ? <Navigate to="/home" /> : <Component />;
+};
+export default UnprotectedRoute;
