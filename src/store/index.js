d18d5428685e1576478bda0d1279cc01f2a44f41
Pboy208 <phuonghoanghaha@gmail.com>
2022-03-30 15:24:40 +0700

    refactor: seperate middlewares to 2 different files

--- src/store/index.js
+++ src/store/index.js
@@ -1,40 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-fallthrough */
 import { configureStore } from '@reduxjs/toolkit';
-import * as Toast from 'components/common/Toast';
 import productReducer from './productSlice';
 import authReducer from './authSlice';
-
-const httpErrorHandlerMiddleware = (store) => (next) => (action) => {
-  if (!action.error) return next(action);
-  const { message, code } = action.error;
-  switch (code) {
-    case '401':
-      store.dispatch({
-        type: 'auth/logout',
-      });
-    case '400':
-    case '404':
-    case '409':
-    case '500':
-      return Toast.error(message);
-    default:
-      console.log('Un-catched status code:::', action);
-      return Toast.error('Unknown error');
-  }
-};
-
-const httpStatusHandlerMiddleware = (store) => (next) => (action) => {
-  console.log(action);
-  const status = action.meta?.requestStatus || null;
-  if (!status) return next(action);
-  const stateName = action.type.split('/')[0];
-  store.dispatch({
-    type: `${stateName}/setIsLoading`,
-    payload: status === 'pending',
-  });
-  return next(action);
-};
+import httpErrorHandlerMiddleware from './httpErrorHandlerMiddleware';
+import httpStatusHandlerMiddleware from './httpStatusHandlerMiddleware';
 
 export const store = configureStore({
   reducer: { product: productReducer, auth: authReducer },
